9c5e89238100dd4bd281b8d486132021e5141801
mkhimself <me@michaelkranker.com>
2017-07-31 23:24:25 -0400

    Index page and Layout file SLICED

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -11,7 +11,7 @@ class Pages extends CI_Controller {
 
     public function index()
     {
-        $this->slice('pages.index');
+        $this->slice->view('pages.index');
     }
 
 }
--- application/views/layouts/default.slice.php
+++ application/views/layouts/default.slice.php
@@ -8,151 +8,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-    <title>Material Design Bootstrap</title>
+    @yield('title')
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
 
     <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ base_url('assets/css/bootstrap.min.css') }}" rel="stylesheet">
 
     <!-- Material Design Bootstrap -->
-    <link href="css/mdb.min.css" rel="stylesheet">
+    <link href="{{ base_url('assets/css/mdb.min.css') }}" rel="stylesheet">
 
     <!-- Template styles -->
-    <link href="{{ base_url('assets/css/main.css') }}" rel="stylesheet">
+    <link href="{{ base_url('assets/css/style.css') }}" rel="stylesheet">
+
+    @yield('page-styles')
 
 </head>
 
 <body>
 
-    <!--Navbar-->
-    <nav class="navbar navbar-toggleable-md navbar-dark fixed-top scrolling-navbar">
-        <div class="container">
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav1" aria-controls="navbarNav1" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <a class="navbar-brand" href="#">
-                <strong>Navbar</strong>
-            </a>
-            <div class="collapse navbar-collapse" id="navbarNav1">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link">About</a>
-                    </li>
-                </ul>
-                <form class="form-inline waves-effect waves-light">
-                    <input class="form-control" type="text" placeholder="Search">
-                </form>
-            </div>
-        </div>
-    </nav>
-    <!--/.Navbar-->
+    @include('includes.nav')
 
-    <!--Carousel Wrapper-->
-    <div id="carousel-example-3" class="carousel slide carousel-fade white-text" data-ride="carousel" data-interval="false">
-        <!--Indicators-->
-        <ol class="carousel-indicators">
-            <li data-target="#carousel-example-3" data-slide-to="0" class="active"></li>
-            <li data-target="#carousel-example-3" data-slide-to="1"></li>
-            <li data-target="#carousel-example-3" data-slide-to="2"></li>
-        </ol>
-        <!--/.Indicators-->
-
-        <!--Slides-->
-        <div class="carousel-inner" role="listbox">
-
-            <!-- First slide -->
-            <div class="carousel-item active view hm-black-light" style="background-image: url('http://mdbootstrap.com/img/Photos/Horizontal/Nature/full%20page/img%20(1).jpg'); background-repeat: no-repeat; background-size: cover;">
-
-                <!-- Caption -->
-                <div class="full-bg-img flex-center white-text">
-                    <ul class="animated fadeInUp col-md-12">
-                        <li>
-                            <h1 class="h1-responsive flex-item">Material Design for Bootstrap 4</h1>
-                            <li>
-                                <p class="flex-item">The most powerful and free UI KIT for Bootstrap</p>
-                            </li>
-                            <li>
-                                <a target="_blank" href="http://mdbootstrap.com/getting-started/" class="btn btn-primary btn-lg flex-item" rel="nofollow">Sign up!</a>
-                            </li>
-                            <li>
-                                <a target="_blank" href="http://mdbootstrap.com/material-design-for-bootstrap/" class="btn btn-default btn-lg flex-item" rel="nofollow">Learn more</a>
-                            </li>
-                    </ul>
-                </div>
-                <!-- /.Caption -->
-
-            </div>
-            <!-- /.First slide -->
-
-            <!-- Second slide -->
-            <div class="carousel-item view hm-black-light" style="background-image: url('http://mdbootstrap.com/img/Photos/Horizontal/Nature/full%20page/img%20(22).jpg'); background-repeat: no-repeat; background-size: cover;">
-
-                <!-- Caption -->
-                <div class="full-bg-img flex-center white-text">
-                    <ul class="animated fadeInUp col-md-12">
-                        <li>
-                            <h1 class="h1-responsive">Lots of tutorials at your disposal</h1>
-                        </li>
-                        <li>
-                            <p>And all of them are FREE!</p>
-                        </li>
-                        <li>
-                            <a target="_blank" href="http://mdbootstrap.com/bootstrap-tutorial/" class="btn btn-primary btn-lg" rel="nofollow">Start learning</a>
-                        </li>
-                    </ul>
-                </div>
-                <!-- /.Caption -->
-
-            </div>
-            <!-- /.Second slide -->
-
-            <!-- Third slide -->
-            <div class="carousel-item view hm-black-light" style="background-image: url('http://mdbootstrap.com/img/Photos/Horizontal/Nature/full%20page/img%20(24).jpg'); background-repeat: no-repeat; background-size: cover;">
-
-                <!-- Caption -->
-                <div class="full-bg-img flex-center white-text">
-                    <ul class="animated fadeInUp col-md-12">
-                        <li>
-                            <h1 class="h1-responsive">Visit our support forum</h1></li>
-                        <li>
-                            <p>Our community can help you with any question</p>
-                        </li>
-                        <li>
-                            <a target="_blank" href="http://mdbootstrap.com/forums/forum/support/" class="btn btn-default btn-lg" rel="nofollow">Support forum</a>
-                        </li>
-                    </ul>
-                </div>
-                <!-- /.Caption -->
-
-            </div>
-            <!-- /.Third slide -->
-            
-        </div>
-        <!--/.Slides-->
-
-        <!--Controls-->
-        <a class="carousel-control-prev" href="#carousel-example-3" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carousel-example-3" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-        <!--/.Controls-->
-    </div>
-    <!--/.Carousel Wrapper-->
+    @yield('content')
     
     <!--/.Main layout-->
 
@@ -235,7 +113,7 @@
         <!--Copyright-->
         <div class="footer-copyright">
             <div class="container-fluid">
-                © 2015 Copyright: <a href="http://www.MDBootstrap.com"> MDBootstrap.com </a>
+                © {{ date('Y') }} <a href="http://www.MDBootstrap.com"> MDBootstrap.com </a>
 
             </div>
         </div>
@@ -248,20 +126,22 @@
     <!-- SCRIPTS -->
 
     <!-- JQuery -->
-    <script type="text/javascript" src="js/jquery-2.2.3.min.js"></script>
+    <script type="text/javascript" src="{{ base_url('assets/js/jquery-2.2.3.min.js') }}"></script>
 
     <!-- Bootstrap tooltips -->
-    <script type="text/javascript" src="js/tether.min.js"></script>
+    <script type="text/javascript" src="{{ base_url('assets/js/tether.min.js') }}"></script>
 
     <!-- Bootstrap core JavaScript -->
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="{{ base_url('assets/js/bootstrap.min.js') }}"></script>
 
     <!-- MDB core JavaScript -->
-    <script type="text/javascript" src="js/mdb.min.js"></script>
+    <script type="text/javascript" src="{{ base_url('assets/js/mdb.min.js') }}"></script>
 
     <script>
-    new WOW().init();
+        new WOW().init();
     </script>
+
+    @yield('pages-scripts')
 
 </body>
 
