aa1e7aba8f6411ed1c58cc143a0f209d14f544e2
mkhimself <me@michaelkranker.com>
2017-07-31 23:06:16 -0400

    Pages controller created and defaulted

---
+++ application/controllers/Pages.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pages extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('slice');
+    }
+
+    public function index()
+    {
+        $this->slice('pages.index');
+    }
+
+}
