0ee91f693835de3843f0c5bf7dd98ceecd42d164
mkhimself <me@michaelkranker.com>
2017-08-01 16:51:47 -0400

    Migration 006 Create works table

--- application/config/migration.php
+++ application/config/migration.php
@@ -74,7 +74,7 @@ $config['migration_auto_latest'] = FALSE;
 | be upgraded / downgraded to.
 |
 */
-$config['migration_version'] = '005';
+$config['migration_version'] = '006';
 
 /*
 |--------------------------------------------------------------------------
