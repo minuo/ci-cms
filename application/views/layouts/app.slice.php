aa3e8bf1b54e723612c2fbee00140092b14b3b70
mkhimself <me@michaelkranker.com>
2017-08-03 08:38:31 -0400

    #A40000 Removed from _all-skins

--- application/views/layouts/app.slice.php
+++ application/views/layouts/app.slice.php
@@ -21,7 +21,7 @@
   <link rel="stylesheet" href="{{ base_url('assets/admin/dist/css/style.css') }}">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="{{ base_url('assets/admin/dist/css/skins/skin-red.min.css') }}">
+  <link rel="stylesheet" href="{{ base_url('assets/admin/dist/css/skins/_all-skins.min.css') }}">
 
   @yield('page-styles')
 
