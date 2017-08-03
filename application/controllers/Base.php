657e90cbf13bb57b5cbd51807c20756ea48fb450
mkhimself <me@michaelkranker.com>
2017-08-03 11:17:23 -0400

    Base controller defaulted instead of Pages

--- application/controllers/Base.php
+++ application/controllers/Base.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Pages extends CI_Controller {
+class Base extends CI_Controller {
 
     public function __construct()
     {
@@ -11,7 +11,7 @@ class Pages extends CI_Controller {
 
     public function index()
     {
-        $this->slice->view('admin.dashboard.pages');
+        $this->slice->view('pages.index');
     }
 
 }
