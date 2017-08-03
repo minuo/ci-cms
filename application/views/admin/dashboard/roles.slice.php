690dcbd5bb02febaf808c7b531f5864b7f1c2cd6
mkhimself <me@michaelkranker.com>
2017-08-03 11:06:08 -0400

    Empty slice views for roles and users

---
+++ application/views/admin/dashboard/roles.slice.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('breadcrumb')
+<section class="content-header">
+    <h1>
+        Roles
+        <small>All</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="{{ base_url('dashboard') }}"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li>Roles</li>
+    </ol>
+</section>
+@endsection
+
+@section('content')
+<div class="box">
+    <div class="box-header">
+        <h3 class="box-title">All Roles</h3>
+        <a href="{{ base_url('dashboard/roles/create') }}" class="btn btn-primary pull-right"><i class="fa fa-plus"></i> Add New</a>
+    </div>
+    <!-- /.box-header -->
+    <div class="box-body">
+
+        <div class="clear-25"></div>
+
+        <table id="dataTable" class="table table-bordered">
+            <thead>
+                <tr>
+                    
+                </tr>
+            </thead>
+            <tbody>
+                       
+            </tbody>
+        </table>
+    </div>
+</div>
+@endsection
+
+@section('page-scripts')
+<!-- DataTables -->
+<script src="{{ base_url('assets/admin/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ base_Url('assets/admin/plugins/datatables/dataTables.bootstrap.min.js') }}"></script>
+
+<script>
+  $(function () {
+    $('#dataTable').DataTable({
+        searching: false,
+        order: [[ 0, "desc" ]]
+    });
+  });
+</script>
+
+@endsection
