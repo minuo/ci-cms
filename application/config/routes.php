5e9e3da27379335e18880bc64d0dbfba213bdaeb
mkhimself <me@michaelkranker.com>
2017-08-02 21:00:16 -0400

    Generic route for posts controller

--- application/config/routes.php
+++ application/config/routes.php
@@ -4,6 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['ci-admin']                      = 'Auth/login';
 $route['ci-admin/dashboard']            = 'Dashboard';
 
+$route['(:any)']                        = 'Posts/index/$1';
+
 $route['default_controller']            = 'Pages';
 $route['404_override']                  = '';
 $route['translate_uri_dashes']          = FALSE;
