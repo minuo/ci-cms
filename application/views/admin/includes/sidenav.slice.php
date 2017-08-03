af6e2904efe7b7cd3bc8b9028c53027e90116c12
mkhimself <me@michaelkranker.com>
2017-08-03 11:18:16 -0400

    Sidnav and Pages index added

--- application/views/admin/includes/sidenav.slice.php
+++ application/views/admin/includes/sidenav.slice.php
@@ -10,7 +10,8 @@
         <li><a href="{{ base_url('ci-admin/posts') }}"><i class="fa fa-pencil-square-o"></i> <span>Posts</span></a></li>
         <li><a href="{{ base_url('ci-admin/comments') }}"><i class="fa fa-comments"></i><span> Comments</span> @php if(isset($pending)): if(count($pending) > 0): echo '<span class="label pull-right bg-green">' . count($pending) . ' New</span>'; endif; endif; @endphp</a></li>
         <li><a href="{{ base_url('ci-admin/users') }}"><i class="fa fa-user"></i><span> Users</span></a></li>
-        
+        <li><a href="{{ base_url('ci-admin/roles') }}"><i class="fa fa-ban"></i><span> Roles</span></a></li>
+
     </ul>
     
 </section>
