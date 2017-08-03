e145a6015fa1966f67272628fb669939b0dd1320
mkhimself <me@michaelkranker.com>
2017-08-03 11:00:00 -0400

    Routes updated

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,17 +1,21 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+// Main Dashboard Routes
 $route['ci-admin']                      = 'Auth/login';
 $route['ci-admin/dashboard']            = 'Dashboard';
 
+// Dashboard Posts Routes
 $route['ci-admin/posts']                = 'Posts';          
 $route['ci-admin/posts/create']         = 'Posts/create';
 $route['ci-admin/posts/store']          = 'Posts/store';
 
+// Dashboard Pages Routes
 $route['ci-admin/pages']                = 'Pages';
 $route['ci-admin/pages/create']         = 'Pages/create';
 $route['ci-admin/pages/store']          = 'Pages/store';
 
+// Default Routes
 $route['default_controller']            = 'Pages';
 $route['404_override']                  = '';
 $route['translate_uri_dashes']          = FALSE;
