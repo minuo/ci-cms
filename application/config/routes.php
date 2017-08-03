83587b9d5ef351576e999dd1850b969e80d13c57
mkhimself <me@michaelkranker.com>
2017-08-03 10:51:45 -0400

    Having (:any) in routes is bad

--- application/config/routes.php
+++ application/config/routes.php
@@ -12,8 +12,6 @@ $route['ci-admin/pages']                = 'Pages';
 $route['ci-admin/pages/create']         = 'Pages/create';
 $route['ci-admin/pages/store']          = 'Pages/store';
 
-$route['(:any)']                        = 'Posts/find/$1';
-
 $route['default_controller']            = 'Pages';
 $route['404_override']                  = '';
 $route['translate_uri_dashes']          = FALSE;
