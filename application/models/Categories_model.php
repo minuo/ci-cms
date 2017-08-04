6d5be7e8f8e569eb948ca9bf6ffd234316645540
mkhimself <me@michaelkranker.com>
2017-08-04 09:04:30 -0400

    Categories Model

--- application/models/Categories_model.php
+++ application/models/Categories_model.php
@@ -4,13 +4,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Categories_model extends CI_Model {
 
     /**
-	* Gets all posts in db
+	* Gets all post_categories in db
 	*
 	*/
     public function get_all()
     {
         $this->db->order_by('id', 'DESC');
-        $query = $this->db->get('posts');
+        $query = $this->db->get('post_categories');
 
         if($query->num_rows() > 0) {
             return $query->result();
@@ -19,13 +19,13 @@ class Categories_model extends CI_Model {
     }
 
     /**
-	* Gets the specified post using posts unique $id
+	* Gets the specified post category using post_categories unique $id
 	*
-	* @param int $id The unique id of post to retrieve
+	* @param int $id The unique id of post category to retrieve
 	*/
-    public function get_post_by_id($id)
+    public function get_category_by_id($id)
     {
-        $query = $this->db->get_where('posts', array('id' => $id));
+        $query = $this->db->get_where('post_categories', array('id' => $id));
 
         if($query->num_rows() > 0)
         {
@@ -35,38 +35,16 @@ class Categories_model extends CI_Model {
     }
 
     /**
-	* Gets the specified post using posts unique $slug
-	*
-	* @param string $slug The unique slug of post to retrieve
-	*/
-    public function get_post_by_guid($guid)
-    {
-        $query = $this->db->get_where('posts', array('guid' => $guid));
-
-        if($query->num_rows() > 0)
-        {
-            return $query->row();
-        }
-
-    }
-
-    /**
-	* Create a new post and save in db
+	* Create a new post category and save in db
 	*
 	*/
     public function create()
     {
         $data = array(            
-            'guid' => url_title($this->input->post('post_title')),
-            'post_type' => $this->input->post('post_type'),
-            'post_author' => $this->session->userdata('id'),
-            'post_title' => $this->input->post('post_title'),
-            'post_body' => $this->input->post('post_body'),
-            'post_status' => $this->input->post('post_status'),
-            'created_at' => date('Y-m-d H:i:s', time())
+      
         );
 
-        $result = $this->db->insert('posts', $data);
+        $result = $this->db->insert('post_categories', $data);
 
         if($result) {
             return true;
@@ -74,23 +52,17 @@ class Categories_model extends CI_Model {
     }
 
     /**
-	* Create a new post and save in db
+	* Create a new post category and save in db
 	*
-    * @param int $id The unique id of post to update
+    * @param int $id The unique id of post category to update
 	*/
     public function update($id)
     {
         $data = array(            
-            'guid' => url_title($this->input->post('post_title')),
-            'post_type' => $this->input->post('post_type'),
-            'post_author' => $this->session->userdata('id'),
-            'post_title' => $this->input->post('post_title'),
-            'post_body' => $this->input->post('post_body'),
-            'post_status' => $this->input->post('post_status'),
-            'updated_at' => date('Y-m-d H:i:s', time())
+
         );
 
-        $result = $this->db->update('posts', $data, array('id' => $id));
+        $result = $this->db->update('post_categories', $data, array('id' => $id));
 
         if($result) {
             return true;
@@ -99,13 +71,13 @@ class Categories_model extends CI_Model {
     }
 
     /**
-	* Deletes the specified post using posts unique $id
+	* Deletes the specified post category using post_categories unique $id
 	*
-	* @param int $id The unique id of post to delete
+	* @param int $id The unique id of post category to delete
 	*/
     public function delete($id)
     {
-        $result = $this->db->delete('posts', array('id' => $id));
+        $result = $this->db->delete('post_categories', array('id' => $id));
         
         if($result) {
             return true;
