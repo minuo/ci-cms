248781d02d77cf7733de8cccf4feb7f020927728
mkhimself <me@michaelkranker.com>
2017-08-03 21:31:19 -0400

    CRUD routes for all controllers

--- application/config/routes.php
+++ application/config/routes.php
@@ -9,21 +9,33 @@ $route['ci-admin/dashboard']                = 'Dashboard';
 $route['ci-admin/posts']                    = 'Posts';          
 $route['ci-admin/posts/create']             = 'Posts/create';
 $route['ci-admin/posts/store']              = 'Posts/store';
+$route['ci-admin/posts/(:num)/edit']        = 'Posts/edit/$1';
+$route['ci-admin/posts/(:num)/update']      = 'Posts/update/$1';
+$route['ci-admin/posts/(:num)/delete']      = 'Posts/destroy/$1';
 
-// Dashboard Posts Routes
+// Dashboard Comments Routes
 $route['ci-admin/comments']                 = 'Comments';          
 $route['ci-admin/comments/create']          = 'Comments/create';
 $route['ci-admin/comments/store']           = 'Comments/store';
+$route['ci-admin/comments/(:num)/edit']     = 'Comments/edit/$1';
+$route['ci-admin/comments/(:num)/update']   = 'Comments/update/$1';
+$route['ci-admin/comments/(:num)/delete']   = 'Comments/destroy/$1';
 
 // Dashboard Pages Routes
 $route['ci-admin/pages']                    = 'Pages';
 $route['ci-admin/pages/create']             = 'Pages/create';
 $route['ci-admin/pages/store']              = 'Pages/store';
+$route['ci-admin/pages/(:num)/edit']        = 'Pages/edit/$1';
+$route['ci-admin/pages/(:num)/update']      = 'Pages/update/$1';
+$route['ci-admin/pages/(:num)/delete']      = 'Pages/destroy/$1';
 
 // Dashboard Users Routes
 $route['ci-admin/users']                    = 'Users';
 $route['ci-admin/users/create']             = 'Users/create';
 $route['ci-admin/users/store']              = 'Users/store';
+$route['ci-admin/users/(:num)/edit']        = 'Users/edit/$1';
+$route['ci-admin/users/(:num)/update']      = 'Users/update/$1';
+$route['ci-admin/users/(:num)/delete']      = 'Users/destroy/$1';
 
 // Dashboard Roles Routes
 $route['ci-admin/roles']                    = 'Roles';
@@ -31,6 +43,7 @@ $route['ci-admin/roles/create']             = 'Roles/create';
 $route['ci-admin/roles/store']              = 'Roles/store';
 $route['ci-admin/roles/(:num)/edit']        = 'Roles/edit/$1';
 $route['ci-admin/roles/(:num)/update']      = 'Roles/update/$1';
+$route['ci-admin/roles/(:num)/delete']      = 'Roles/destroy/$1';
 
 // Default Routes
 $route['default_controller']                = 'Base';
