fbdf379b95bc6649a6002608fd280647e641d137
itzJustKranker <me@michaelkranker.com>
2017-09-15 16:02:05 -0400

    Add->Edit on Edit role page

--- application/views/admin/edit/role.slice.php
+++ application/views/admin/edit/role.slice.php
@@ -8,12 +8,12 @@
 <section class="content-header">
     <h1>
         Roles
-        <small>Add</small>
+        <small>Edit</small>
     </h1>
     <ol class="breadcrumb">
         <li><a href="{{ base_url('ci-admin') }}"><i class="fa fa-dashboard"></i> Home</a></li>
         <li><a href="{{ base_url('ci-admin/roles') }}"><i class="fa fa-ban"></i> Roles</a></li>
-        <li>Add</li>
+        <li>Edit</li>
     </ol>
 </section>
 @endsection
@@ -25,7 +25,7 @@
         <div class="col-md-8">            
             <div class="box box-danger">
                 <div class="box-header with-border">
-                    <h3 class="box-title">Add Role</h3>
+                    <h3 class="box-title">Edit {{ $role->type_name }} Role</h3>
                 </div>
                 <!-- /.box-header -->            
                 <div class="box-body">
